139b5d7b7ad3bca7137ee6e8ed3ddd6c03af0817
Rémy <158462966+remyd06@users.noreply.github.com>
2026-03-20 21:28:13 +0100

    Add the task who take the signal in the led and speaker queue and translate each "-" "." "|" to a signal who SET and RESET the rights pins.

---
+++ Core/Inc/task_led_and_speaker.h
@@ -0,0 +1,15 @@
+/*
+ * task_led_and_speaker.h
+ *
+ *  Created on: 20 mars 2026
+ *      Author: remyd
+ */
+
+#ifndef INC_TASK_LED_AND_SPEAKER_H_
+#define INC_TASK_LED_AND_SPEAKER_H_
+
+# include "globals_var.h"
+
+void	task_led_and_speaker(void *argument);
+
+#endif /* INC_TASK_LED_AND_SPEAKER_H_ */
---
+++ Core/Src/task_led_and_speaker.c
@@ -0,0 +1,86 @@
+/*
+ * task_led_and_speaker.c
+ *
+ *  Created on: 20 mars 2026
+ *      Author: remyd
+ */
+
+#include "task_led_and_speaker.h"
+
+void	set_buffer_led_and_speaker(char	encoded_buffer[][7])
+{
+	char		q_byte = 0;
+	size_t		encoded_index = 0;
+	size_t		encoded_index_line = 0;
+	char		sep = '\0';
+
+	while (1)
+	{
+		xQueueReceive(xQueueLedAndSpeaker, &q_byte, portMAX_DELAY);
+
+		if (q_byte != '\r' && q_byte != '\0')
+			encoded_buffer[encoded_index_line][encoded_index++] = q_byte;
+		else
+		{
+			if (q_byte == '\0')
+			{
+				encoded_buffer[encoded_index_line][encoded_index] = q_byte;
+				break ;
+			}
+			else
+			{
+				encoded_buffer[encoded_index_line++][encoded_index] = sep;
+				encoded_index = 0;
+			}
+		}
+	}
+}
+
+void	task_led_and_speaker(void *argument)
+{
+	char	encoded_buffer[64][7];
+
+	set_buffer_led_and_speaker(encoded_buffer);
+	for (;;)
+	{
+		size_t		encoded_index = 0;
+		size_t		encoded_index_line = 0;
+
+
+		while (encoded_buffer[encoded_index_line][0])
+		{
+			while (encoded_buffer[encoded_index_line][encoded_index])
+			{
+				if (encoded_buffer[encoded_index_line][encoded_index] == '.')
+				{
+					HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+					HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_1);
+					osDelay(gState.SPEED);
+					HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+					HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_1);
+					osDelay(gState.SPEED);
+				}
+				else if (encoded_buffer[encoded_index_line][encoded_index] == '-')
+				{
+					HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+					HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_1);
+					osDelay(gState.SPEED * 3);
+					HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+					HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_1);
+					osDelay(gState.SPEED);
+				}
+				else
+					osDelay(gState.SPEED * 4);
+
+				osDelay(gState.SPEED);
+				encoded_index++;
+			}
+			osDelay(gState.SPEED * 3); // entre lettre
+			encoded_index = 0;
+			encoded_index_line++;
+		}
+		osDelay(gState.SPEED * 7);
+//		memset(encoded_buffer, 0, sizeof(encoded_buffer));
+
+	}
+}
